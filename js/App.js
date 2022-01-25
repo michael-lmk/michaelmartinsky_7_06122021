5a5deaeaae7bca70b51fb8b798eaeb10cdf4e866
michael-lmk <>
2022-01-25 16:18:53 +0100

    Affichage des recettes version design partern

--- js/App.js
+++ js/App.js
@@ -3,15 +3,30 @@ import { recipes } from "/js/data/recipes.js";
 
 class App {
     constructor() {
-        this.$indexMain = document.querySelector('#main');
+        this.mainContent = document.getElementById("main");
         this.recipes = [];
     }
 
     async index() {
-        
         // je récupère les data de mon fichier 
-        // const receipesData = await this.RecipeApi.getRecipes();
-        console.log(recipes);
+        // recipes.map(async(recipe,i) => {
+        for (let index = 0; index < recipes.length; index++) {
+            var recipe = recipes[index];
+            if ((index % 4) === 0) {
+                
+                var element = document.createElement("div");    
+                element.setAttribute("class","card-row row row-cols-1 row-cols-md-3 row-cols-xl-4 g-4")
+            }
+
+            const recipeClass = new Recipe(recipe);
+            const result = await recipeClass.buildCard();
+            element.appendChild(result)
+            this.recipes.push(recipeClass);
+            this.mainContent.appendChild(element)
+        }
+            
+
+        // })
     }
     
 }
