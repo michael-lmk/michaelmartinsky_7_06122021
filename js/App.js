cf489511b8b09f0081674fe645a9deb1071adc7d
michael-lmk <>
2022-02-01 16:45:15 +0100

    Ajout de la recherhce des ingrédients

--- js/App.js
+++ js/App.js
@@ -1,36 +1,73 @@
-
 import { recipes } from "/js/data/recipes.js";
 
 class App {
-    constructor() {
-        this.mainContent = document.getElementById("main");
-        this.recipes = [];
-    }
+  constructor() {
+    this.mainContent = document.getElementById("main");
+    this.recipes = [];
+    this.ingredients = [];
+  }
 
-    async index() {
-        // je récupère les data de mon fichier 
-        // recipes.map(async(recipe,i) => {
-        for (let index = 0; index < recipes.length; index++) {
-            var recipe = recipes[index];
-            if ((index % 4) === 0) {
-                
-                var element = document.createElement("div");    
-                element.setAttribute("class","card-row row row-cols-1 row-cols-md-3 row-cols-xl-4 g-4")
-            }
+  async index() {
+    // je récupère les data de mon fichier
+    for (let index = 0; index < recipes.length; index++) {
+      var recipe = recipes[index];
+      if (index % 3 === 0) {
+        var element = document.createElement("div");
+        element.setAttribute(
+          "class",
+          "card-row row row-cols-1 row-cols-md-3 row-cols-xl-3 g-4"
+        );
+      }
 
-            const recipeClass = new Recipe(recipe);
-            const result = await recipeClass.buildCard();
-            element.appendChild(result)
-            this.recipes.push(recipeClass);
-            this.mainContent.appendChild(element)
+      const recipeClass = new Recipe(recipe);
+      const result = await recipeClass.buildCard();
+      element.appendChild(result);
+      this.recipes.push(recipeClass);
+      this.mainContent.appendChild(element);
+
+      for ( let indexBis = 0; indexBis < recipeClass.ingredients.length;indexBis++ ) {
+
+        const ingredient = recipeClass.ingredients[indexBis].ingredient.toLowerCase();
+        const domParent = document.getElementById("data_list_ingre");
+
+        if (this.ingredients.indexOf(ingredient.toLowerCase()) === -1) {
+          this.ingredients.push(ingredient.toLowerCase());
+          var li = document.createElement("li");
+          li.setAttribute("class","ingredient");
+          li.innerText = ingredient;
+          domParent.appendChild(li);
         }
-            
-
-        // })
+        
+         
+      }
     }
-    
+  }
+
+  search_ingredients() {
+    let input = document.getElementById("input_search_ingre").value;
+
+    input = input.split(",").pop().trim()
+
+    let domIngredients = document.getElementsByClassName("ingredient");
+
+    for (var i = 0; i < domIngredients.length; i++) {
+
+      if (!domIngredients[i].innerHTML.toLowerCase().includes(input)) {
+        domIngredients[i].style.display = "none";
+      } else {
+        domIngredients[i].style.display = "list-item";
+      }
+    }
+  }
 }
 
 const app = new App();
 
 app.index();
+
+const searchInputIngre = document.getElementById("input_search_ingre");
+searchInputIngre.addEventListener("input", (e) => {
+  app.search_ingredients();
+
+});
+
--- js/action/index.js
+++ js/action/index.js
@@ -1,28 +1,47 @@
 const input_drop_downs = document.getElementsByClassName("find_btn");
 
-function buildDropDown () {
-    
-}
+function buildDropDown() { }
 
+const changeStateDropDown = (index,e) => {
+  
+    var dropDown = document.getElementById(`drop_down_${index + 1}`);
+    var imgDropDown = `img_drop_down_${index + 1}`;
+   
+    var findDivContainer = document.getElementsByClassName(`find_div_container_${index + 1}`)[0];
+
+    if (findDivContainer.contains(e.target)) {
+
+      dropDown.style.display = "block";
+      document.getElementsByClassName(imgDropDown)[0].style.transform = "rotate(180deg)";
+      var dropDownWidth = dropDown.clientWidth ;
+      findDivContainer.style.flex = "1";
+
+    } else {
+
+      dropDown.style.display = "none";
+      document.getElementsByClassName(imgDropDown)[0].style.transform = "rotate(0deg)";
+      findDivContainer.style.flex = "0";
+
+    }
+
+}
 
 for (let index = 0; index < input_drop_downs.length; index++) {
-    const element = input_drop_downs[index];
-
-    element.addEventListener("focus", (e) => {
-
-        var test = `drop_down_${index+1}`;
-        
-        document.getElementById(test).style.display = "block";
-       
-    })
-
-    element.addEventListener("blur", (e) => {
-        var test = `drop_down_${index+1}`;
-        
-        document.getElementById(test).style.display = "none";
-    })
+  window.addEventListener("click", (e) => changeStateDropDown(index,e))
 }
 
+var listAutocomplete = document.getElementsByClassName("ingredient")
+for (let index = 0; index < listAutocomplete.length; index++) {
+  var li = listAutocomplete[index];
 
+  li.addEventListener("click", function (e) {
+    var selected = e.target.innerText;
+    var inputValue = document.getElementById("input_search_ingre");
+    var arrayInputValue = inputValue.value.split(",");
+    
+    arrayInputValue[arrayInputValue.length-1] = selected;
+    
+    inputValue.value = arrayInputValue.join(",")+","; 
 
-buildCard();
+  })  
+};
